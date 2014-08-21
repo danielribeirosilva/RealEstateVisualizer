7aecb1a96a07c7564616e266a86e31ba49d44a76
Daniel <danielsilva.ita@gmail.com>
2014-08-21 18:05:53 -0300

    start of neighborhood checkbox filter

--- ajax/getAllNeighborhoods.php
+++ ajax/getAllNeighborhoods.php
@@ -11,8 +11,10 @@ $property_type = $DEFAULT_PROPERTY_TYPE;
 
 if(isset($_GET['city'])){
 	//TODO: sanitize
-	$city = $_GET['city'];
-	$city = html_entity_decode($city);
+	// $city = $_GET['city'];
+	// $city = html_entity_decode($city);
+	// $property_type = $_GET['property_type'];
+	// $property_type = html_entity_decode($property_type);
 }
 if(isset($_GET['property_type'])){
 	//TODO: sanitize
@@ -20,16 +22,18 @@ if(isset($_GET['property_type'])){
 	$property_type = html_entity_decode($property_type);
 }
 
-echo json_encode(getAllProperties($city, $property_type));
+echo json_encode(getAllNeighborhoods($city, $property_type));
 
-function getAllProperties($city, $property_type){
+function getAllNeighborhoods($city, $property_type){
 	$dbh = Database::connect();
-	$query = "SELECT * FROM properties WHERE `city` = '" . $city . "' AND `property_type` = '" . $property_type . "'";
+	$query = "SELECT DISTINCT(neighborhood) AS neighborhood FROM properties
+			  WHERE `city` = '" . $city . "' AND `property_type` = '" . $property_type . "'
+			  ORDER BY neighborhood";
 	$sth = $dbh->prepare($query);
 	$sth->execute();
 	$res = array() ;
 	for($i = 0 ; $i < $sth->rowCount() ; $i++){
-		$res[$i] = $sth->fetch();
+		$res[$i] = $sth->fetch()['neighborhood'];
 	}
 	return $res;
 }
--- js/app.js
+++ js/app.js
@@ -2,37 +2,61 @@
 var app = angular.module('myApp', ['ui-rangeSlider']);
 window.DEFAULT_CITY = 'Maringá';
 window.DEFAULT_PROPERTY_TYPE = 'TERRENO';
+//TODO: get from URL
+window.city = DEFAULT_CITY;
+window.property_type = DEFAULT_PROPERTY_TYPE;
 
 
 app.controller('dataController', function($scope, $http) {
-
-	//prepare range filters
-	$scope.rangeFilters = getRangeFilters();
-
 	
-	//cache data
-	getAllProperties(DEFAULT_CITY, DEFAULT_PROPERTY_TYPE, function(data){
-		$scope.properties = function(){return getFilteredProperties($scope.cachedProperties);};
+	//first of all, cache all the data (creates $scope.cachedProperties)
+	getAllProperties(DEFAULT_CITY, DEFAULT_PROPERTY_TYPE, function(){
+		//then, get all neighborhoods (creates $scope.neigborhoods)
+		getAllNeighborhoods(DEFAULT_CITY, DEFAULT_PROPERTY_TYPE, function(){
+
+			//prepare range filters
+			$scope.rangeFilters = getRangeFilters();
+
+			//prepare checkbox filters
+			$scope.checkBoxFilters = getCheckBoxFilters($scope.neighborhoods);
+
+			//get data after filtering
+			$scope.getConsideredProperties = function(){return getConsideredProperties($scope.cachedProperties);};
+			
+			//stats variables
+			$scope.totalPropertiesOverall = $scope.cachedProperties.length;
+			$scope.totalPropertiesConsidered = function(){return $scope.getConsideredProperties().length;};
+		});
 	});
 
 	
 
+
 	function getLowestValue(data, field){
-		_.min(data, function(d){return d[field];});
+		return _.min(data, function(d){return parseFloat(d[field]);})[field];
 	}
 
 	function getHighestValue(data, field){
-		_.max(data, function(d){return d[field];});
+		return _.max(data, function(d){return parseFloat(d[field]);})[field];
 	}
 
-	function getFilteredProperties(cachedProperties){
-		console.log('filtering...');
+
+	//-------------------
+	//		FILTERS
+	//-------------------
+
+	//apply all filters
+	function getConsideredProperties(cachedProperties){
 		var filteredData;
+		
+		//filter by range filters
 		filteredData = filterByRangeFilter($scope.rangeFilters, cachedProperties);
-		console.log(filteredData);
+
+
 		return filteredData;
 	}
 
+	//apply filter to data
 	function filterByRangeFilter(rangeFilters, data){
 		var filteredData = [];
 		for(var i=0; i<data.length; i++){
@@ -40,6 +64,14 @@ app.controller('dataController', function($scope, $http) {
 
 			for(var j=0; j<rangeFilters.length; j++){
 				var elementValue = getValue(data[i],rangeFilters[j].dbName);
+				//if pointer is on upBound, don't filter high values
+				if(rangeFilters[j].lowValue < elementValue && rangeFilters[j].upValue == rangeFilters[j].upBound){
+					continue;
+				}
+				//same for lower bound
+				if(rangeFilters[j].upValue > elementValue && rangeFilters[j].lowValue == rangeFilters[j].lowBound){
+					continue;
+				}
 				//if doesnt pass filter
 				if(rangeFilters[j].lowValue > elementValue || rangeFilters[j].upValue < elementValue){
 					passesFilter = false;
@@ -55,43 +87,79 @@ app.controller('dataController', function($scope, $http) {
 	}
 
 
+	function getRangeFilters(){
+		var rangeFilters = [];
+		//DISTANCE TO CENTER
+		//var distanceToCenterFilter = {"name":"distancia ao centro","dbName":"distance_center","unit":"km","lowBound":0,"upBound":10,"lowValue":2,"upValue":4,"decimalPlaces":1};
+		//rangeFilters.push(distanceToCenterFilter);
+		//PROPERTY SIZE
+		var propertySizeFilter = {"name":"tamanho do terreno","dbName":"area_total","unit":"m\xB2","lowBound":0,"upBound":1000,"lowValue":200,"upValue":800,"decimalPlaces":0};
+		rangeFilters.push(propertySizeFilter);
+		//PRICE PER AREA
+		var propertyPricePerAreaFilter = {"name":"preco por m\xB2","dbName":"price/area_total","unit":"R$/m\xB2","lowBound":0,"upBound":800,"lowValue":100,"upValue":1000,"decimalPlaces":0};
+		rangeFilters.push(propertyPricePerAreaFilter);
+		return rangeFilters;
+	}
+
+	function getCheckBoxFilters(neighborhoods){
+		var checkBoxFilters = [];
+		//NEIGHBORHOODS
+		var neighborhoodFilter = {"name":"vizinhancas", "data":neighborhoods};
+		checkBoxFilters.push(neighborhoodFilter);
+
+		return checkBoxFilters;
+	}
+
+	//very stupid and restricted parser to get the value of dbName in element.
+	//the idea is that it is able to handle cases of simple ratios (/) and multiplications (*)
+	function getValue(element, dbName){
+		if(dbName.indexOf('/')>=0){
+			var splitDbName = dbName.split('/');
+			if(element[splitDbName[1]] == 0){return 0;}
+			return element[splitDbName[0]] / element[splitDbName[1]];
+		}
+		if(dbName.indexOf('*')>=0){
+			var splitDbName = dbName.split('*');
+			return element[splitDbName[0]] * element[splitDbName[1]];
+		}
+		return element[dbName];
+	}
+
+
+	//-------------------
+	//		AJAX
+	//-------------------
+
 	//main data request is based on (city, property_type). 
 	//All matching data is cached, so that user-side filtering doesn't need any new data request.
 	function getAllProperties(city, property_type, callback){
+		console.log('querying db ...');
 		$http.get("ajax/getAllProperties.php?city="+city+"&property_type="+property_type).success(function(data){
-			console.log(data);
 			$scope.cachedProperties = data;
-			callback(data);
+			callback();
+		});
+	};
+
+	//data request for neighborhood list for given (city, property_type).
+	function getAllNeighborhoods(city, property_type, callback){
+		console.log('querying db ...');
+		$http.get("ajax/getAllNeighborhoods.php?city="+city+"&property_type="+property_type).success(function(data){
+			var neighborhoodData = [];
+			//activate all initially
+			for(var i=0; i<data.length; i++){
+				neighborhoodData.push({'option':data[i],'active':true});
+			}
+			$scope.neighborhoods = neighborhoodData;
+			callback();
 		});
 	};
  
-});
+});//end of app
 
 
 
-//very stupid and restricted parser to get the value of dbName in element.
-//the idea is that it is able to handle cases of simple ratios (/) and multiplications (*)
-function getValue(element, dbName){
-	if(dbName.indexOf('/')>=0){
-		var splitDbName = dbName.split('/');
-		return element[splitDbName[0]] / element[splitDbName[1]];
-	}
-	if(dbName.indexOf('*')>=0){
-		var splitDbName = dbName.split('*');
-		return element[splitDbName[0]] * element[splitDbName[1]];
-	}
-	return element[dbName];
-}
 
-function getRangeFilters(){
-	var rangeFilters = [];
-	//var distanceToCenterFilter = {"name":"distancia ao centro","dbName":"distance_center","unit":"km","lowBound":0,"upBound":10,"lowValue":2,"upValue":4,"decimalPlaces":1};
-	//rangeFilters.push(distanceToCenterFilter);
-	var propertySizeFilter = {"name":"tamanho do terreno","dbName":"area_total","unit":"m\xB2","lowBound":0,"upBound":1000,"lowValue":200,"upValue":800,"decimalPlaces":0};
-	rangeFilters.push(propertySizeFilter);
-	var propertyPricePerAreaFilter = {"name":"preco por m\xB2","dbName":"price/area_total","unit":"R$/m\xB2","lowBound":0,"upBound":1000,"lowValue":100,"upValue":1000,"decimalPlaces":0};
-	rangeFilters.push(propertyPricePerAreaFilter);
-	return rangeFilters;
-}
+
+
 
 
